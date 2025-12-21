310040c36b5cdd351af78551bd46649606261637
JanDuchscherer104 <janduchscher@gmail.com>
2025-12-21 19:01:00 +0100

    add doc classifier to app

--- traenslenzor/app/app.py
+++ traenslenzor/app/app.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from traenslenzor.doc_class_detector.mcp import run as run_doc_class_detector
+from traenslenzor.doc_classifier.mcp_integration.mcp_server import run as run_doc_classifier
 from traenslenzor.file_server.server import run as run_file_server
 from traenslenzor.font_detector.mcp import run as run_font_detector
 from traenslenzor.image_renderer.mcp import run as run_image_renderer
@@ -17,7 +17,8 @@ def run():
         loop.create_task(run_image_renderer()),
         loop.create_task(run_text_extractor()),
         loop.create_task(run_translator()),
-        loop.create_task(run_doc_class_detector()),
+        # loop.create_task(run_doc_class_detector()),
+        loop.create_task(run_doc_classifier()),
         loop.create_task(run_font_detector()),
         loop.create_task(run_streamlit()),  # Must be last or weird error :D
     ]
